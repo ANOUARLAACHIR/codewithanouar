ca5bf5059ae4bf7a7bc99c9b05c507e368711f49
ANOUARLAACHIR <become.engineer88@gmail.com>
2023-03-11 21:53:29 +0100

    update index and footer

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+  <script src="<?php echo "/codewithanouar/js/jquery.min.js"; ?>"></script>
+  <script src="<?php echo "/codewithanouar/js/popper.js"; ?>"></script>
+  <script src="<?php echo "/codewithanouar/js/bootstrap.min.js"; ?>"></script>
+  <script src="<?php echo "/codewithanouar/js/main.js"; ?>"></script>
+</body>
+</html>
